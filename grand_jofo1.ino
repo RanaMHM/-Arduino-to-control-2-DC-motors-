4260677e2d7e56103083f0cfd57f02b7c9574bee
RanaMHM <52053143+RanaMHM@users.noreply.github.com>
2021-08-08 13:01:29 +0300

    Update grand_jofo1.ino

--- grand_jofo1.ino
+++ grand_jofo1.ino
@@ -12,22 +12,10 @@ void setup()
 void loop()
 {
   digitalWrite(13, HIGH);
- 
-  digitalWrite(13, LOW);
-  
-  
-  digitalWrite(12, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
   digitalWrite(12, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
-  
-  digitalWrite(7, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
+
+    digitalWrite(8, HIGH);
   digitalWrite(7, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
+
   
-  digitalWrite(8, HIGH);
-  delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(8, LOW);
-  delay(1000); // Wait for 1000 millisecond(s)
 }
